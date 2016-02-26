7669e3881ff2d30d88d2faf61eaeccdd182d6557
Patience <chenps10@gmail.com>
2016-02-26 21:36:48 +0800

    数组与枚举学习过程记忆

---
+++ array/array/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  array
+//
+//  Created by 陈培升 on 16/2/26.
+//  Copyright © 2016年 陈培升. All rights reserved.
+//
+
+import Foundation
+
+//var array=[Int]()
+//array.append(100)
+//print(array[0])
+//var arr=[String]()
+//arr.append("chenps")
+//print(arr[0])
+//var arr=[NSObject]()
+//arr.append("chenps")
+//arr.append(100)
+//arr.append("helloworld")
+//print(arr)
+var arr=[[Int]]()
+arr=[
+    [1,2,3,4],
+    [5,6,7,8]
+]
+print(arr[1][0])
---
+++ enum/enum/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  enum
+//
+//  Created by 陈培升 on 16/2/26.
+//  Copyright © 2016年 陈培升. All rights reserved.
+//
+
+import Foundation
+
+//enum Direction:Int{
+//    case up=1
+//    case down=2
+//    case left=3
+//    case right=4
+//}
+//
+//print(Direction.up)
+//print(Direction.up.hashValue)
+//print(Direction.up.rawValue)
+
+
+//enum Direction:String{
+//    case name="chenps"
+//    case age="20"
+//}
+//
+//print(Direction.age.rawValue)
+//print(Direction.name.rawValue)
+
